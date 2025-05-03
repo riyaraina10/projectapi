4317bbb9a6f326e9eb30225e155d60663ead9ef6
riyaraina10 <riya.raina10@gmail.com>
2025-05-03 16:27:53 +0530

    fixed the prop issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,49 +8,44 @@ const MEAL_BYID_API = import.meta.env.VITE_MEAL_BYID_API;
 const SEARCH_API = import.meta.env.VITE_SEARCH_MEAL_API;
 
 const Home = () => {
-  const [randomMeal, setRandomMeal] = useState(null);
-  const [favoriteMeals, setFavoriteMeals] = useState([]);
-  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
-  //console.log(RANDOM_API)
+    const [randomMeal, setRandomMeal] = useState(null);
+    const [favoriteMeals, setFavoriteMeals] = useState([]);
+    const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+    //console.log(RANDOM_API)
 
-  useEffect(() => {
-    loadRandomMeal();
-  },[])
+    useEffect(() => {
+        loadRandomMeal();
+    }, []);
 
-  const loadRandomMeal = async () => {
-    const resp = await fetch(RANDOM_API);
-    const data = await resp.json();
-    let meal = data.meals[0];
-    console.log(meal);
-    setRandomMeal(meal);
-  }
+    const loadRandomMeal = async () => {
+        const resp = await fetch(RANDOM_API);
+        const data = await resp.json();
+        let meal = data.meals[0];
+        console.log(meal);
+        setRandomMeal(meal);
+    };
 
-  const getMealById = async (id) => {
-    const resp = await fetch(MEAL_BYID_API + id);
-    const data = await resp.json();
-    let meal = data.meals[0];
-    //console.log(meal);
-    return meal;
-  }
+    const getMealById = async (id) => {
+        const resp = await fetch(MEAL_BYID_API + id);
+        const data = await resp.json();
+        let meal = data.meals[0];
+        //console.log(meal);
+        return meal;
+    };
 
-  
-  return (
-    <div className="store">
-    <Search/>
-    
-    <Favorites />
-    
-    <div className="meals" id="meals">
-      {randomMeal && (
-      <MealCard 
-        mealData={randomMeal}
-        isRandom={true}
-      />
-      )}
-    </div>
-    
-  </div>
-  )
-}
+    return (
+        <div className="store">
+            <Search />
 
-export default Home
+            <Favorites favoriteMeals={favoriteMeals} />
+
+            <div className="meals" id="meals">
+                {randomMeal && (
+                    <MealCard mealData={randomMeal} isRandom={true} />
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
